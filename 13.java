f3223306d3f3ad823dca47799ddfc5715b179735
Yuxiang Wu <yuxianw5@uci.edu>
2018-11-08 09:00:31 -0800

    fix the error and add some comments for problem 13

--- 13.java
+++ 13.java
@@ -1,4 +1,11 @@
-    public static int romanToInt(String s){
+//有错，回头改
+//改好了
+/**
+ 思路：罗马数字“AB”，如果A<B，则表示值为B-A; A>B，表现值为A+B
+ 那么干脆一个一个字符地读，如果当前字符的值大于前一个字符的值就表示前一个字符表示的是剪去它的意思
+ 由于之前读到前一个字符的时候已经加了它的值，现在需要减两倍
+**/
+    public int romanToInt(String s){
         if(s==null) return 0;
 
         int prev = getValue(s.charAt(0));
@@ -7,12 +14,14 @@
         for(int i = 1; i<s.length(); i++){
             int cur = getValue(s.charAt(i));
             res += cur > prev ? cur - 2*prev : cur;
+            //忘了加这一句
+            prev = cur;
         }
         return res;
 
     }
 
-    public static int getValue(char c){
+    public int getValue(char c){
         switch (c){
             case 'I':
                 return 1;
