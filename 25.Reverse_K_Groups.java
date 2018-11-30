fe8fb88bf4a7ce85f7042772a9559cd6cc32377c
Yuxiang Wu <yuxianw5@uci.edu>
2018-11-29 16:39:27 -0800

    add some comments

--- 25.Reverse_K_Groups.java
+++ 25.Reverse_K_Groups.java
@@ -1,3 +1,7 @@
+//读取k个元素，把他们的值分别存入数组，当k个值存满，将这k个ListNode的val按照reverse数组的反方向赋值
+//有意思的地方在于保存prev指向k个ListNode的前面一个ListNode
+//dummy的概念一直都很有用
+//T(n) = O(n)
 	public ListNode reverseKGroup(ListNode head, int k) {
         if(head == null) return null;
         int[] reverse = new int[k];
@@ -8,10 +12,8 @@
         while(dummy.next != null){
             dummy = dummy.next;
             reverse[count++] = dummy.val;
-            //System.out.println("dummy = "+dummy.val);
             if(count==k){
                 //Do the reverse
-                //System.out.println("count "+count);
                 count = 0;
                 prev = prev.next;
                 for(int i = 0; i<k; i++){
@@ -19,7 +21,6 @@
                     prev = prev.next;
                 }
                 prev = dummy;
-                //System.out.println("prev = "+prev.val);
             }
         }
         return head;
